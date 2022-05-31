9a5c02bd10c265f6bc28ddb02f76a980e739b177
laura nascimento <goulartlau@gmail.com>
2022-05-31 11:47:59 -0300

    resolvido ex-08 seção 02

--- secao_02_estrutura_de_decisao/ex_08_escolha_de_produto.py
+++ secao_02_estrutura_de_decisao/ex_08_escolha_de_produto.py
@@ -20,3 +20,11 @@ Mostrar o resultado com duas casas decimais
 
 def decidir_melhor_produto(x, y, z):
     """Escreva aqui em baixo a sua solução"""
+    menor= x 
+    if y < x and y < z:
+        menor=y
+    if z < x and z < y:
+        menor=z 
+    print(f'Melhor produto custa R$ {menor:.2f}')
+
+
